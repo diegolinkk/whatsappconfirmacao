e3ea4edecc8c32a72221fea7ea5b2525885d3ace
diegolinkk <diegolinkk@gmail.com>
2019-04-08 16:30:33 -0300

    primeiro commit

---
+++ js/dadosUnidades.js
@@ -0,0 +1,116 @@
+﻿//variaveis globais e com informações de cada unidade
+
+function carregarDadosUnidade(unidade){
+	switch(unidade){
+		case "tatuape":
+			dadosTA();
+			break;
+		case "tatuape2":
+			dadosTA2();
+			break;
+		case "santaCruz":
+			dadosST();
+			break;
+		case "saoCaetano":
+			dadosSC();
+			break;
+		case "lapa":
+			dadosLA();
+			break;
+		case "santoAndre":
+			dadosSA();
+			break;
+		case "santoAmaro":
+			dadosSM();
+			break;
+		default:
+			dadosTA();
+	}
+	carregarListaMedicos();
+}
+
+function carregarListaMedicos(){
+	//carrega a select com os nomes dos médicos
+	var listaMedicos = document.querySelector("#medicos");
+	listaMedicos.innerHTML=""; //limpa a lista para preencher novamente
+	
+	//preenche lista
+	medicos.forEach(function(medico,indice){
+		 var optionMedico = document.createElement("option");
+		 optionMedico.value = medico;
+		 optionMedico.textContent = medico;
+		 listaMedicos.appendChild(optionMedico);
+	});
+}
+
+//funções apenas com dados das unidades
+function dadosTA(){
+	nomeUnidade = "Tatuapé";
+	enderecoUnidade = "Rua tuiuti, 1471 - Tatuapé";
+	referenciaUnidade = "(Próximo Metrô Tatuapé, sentido Av. Celso Garcia. Em frente ao Colégio Espírito Santo)";
+	telUnidade = "11 2942-1833";
+	medicos = [
+		"Dr.Tatuapé1",
+		"Dr.Tatuapé2"
+	];
+}
+function dadosTA2(){
+	nomeUnidade = "Tatuapé";
+	enderecoUnidade = "Gonçalves Crespo, 220 - Tatuapé";
+	referenciaUnidade = "(Próximo Metrô Tatuapé, sentido Av. Celso Garcia. Em frente ao Colégio Espírito Santo)";
+	telUnidade = "11 2942-1833";
+	medicos = [
+		"Dr.Pr21",
+		"Dr.Pr22"
+	];
+}
+function dadosST(){
+	nomeUnidade = "Santa Cruz";
+	enderecoUnidade = "Rua Borges Lagoa, 85";
+	referenciaUnidade = "(Próximo Metrô Santa Cruz, e ao Habib's)";
+	telUnidade = "11 3149-2888";
+	medicos = [
+		"Dra.Santa Cruz",
+		"Dra.Santa Cruz2"
+	];
+}
+function dadosSC(){
+	nomeUnidade = "São Caetano";
+	enderecoUnidade = "Rua Niterói, 237";
+	referenciaUnidade = "(Próximo à estação São Caetano e ao terminal de ônibus)";
+	telUnidade = "11 4228-7344";
+	medicos = [
+		"Dr.São Caetano",
+		"Dr.São Caetano2"
+	];
+}
+function dadosLA(){
+	nomeUnidade = "Lapa";
+	enderecoUnidade = "Rua Tomé de Souza, 186";
+	referenciaUnidade = "(Próximo ao terminal Lapa)";
+	telUnidade = "11 3649-0900";
+	medicos = [
+		"Dr.Lapa",
+		"Dr.Lapa2"
+	];
+}
+function dadosSA(){
+	nomeUnidade = "Santo André";
+	enderecoUnidade = "Rua das Monções, 356";
+	referenciaUnidade = "(Próximo ao Mc Donalds)";
+	telUnidade = "11 4433-4133";
+	medicos = [
+		"Dr.Santo André",
+		"Dr Santo André2"
+	];
+}
+function dadosSM(){
+	nomeUnidade = "Santo Amaro";
+	enderecoUnidade = "Av. Santo Amaro, 6257";
+	referenciaUnidade = "(Próximo à estação Borba Gato)";
+	telUnidade = "11 5682-8711";
+	medicos = [
+		"Dr.Santo Amaro",
+		"Dr.Santo Amaro2"
+	];
+}
---
+++ js/enviar.js
@@ -0,0 +1,62 @@
+//carregando dados das unidades
+var selectUnidade = document.querySelector("#selectUnidades");
+carregarDadosUnidade(selectUnidade.value);
+
+selectUnidade.addEventListener("change",function(){
+	//quando muda unidade, recarrega os dados da unidade
+	carregarDadosUnidade(this.value);
+});
+
+var formulario = document.querySelector("#formulario");
+var enviar = formulario.querySelector("#enviar");
+
+enviar.addEventListener("click",enviarDados); //evento de enviar dados
+
+function enviarDados(){
+	event.preventDefault(); //parando o enviar padrão do form
+	var medico = formulario.querySelector("#medicos").value; //funcionando
+	var data = formatarData(formulario.querySelector("#data").value); //deixa data correta - funcionando
+	var hora = formulario.querySelector("#hora").value;
+	var tipoDeAtendimento = formulario.querySelector("#tipoDeAtendimento").value;
+	console.log(tipoDeAtendimento);
+	var telefonePaciente = "phone=5511" + formulario.querySelector("#telefonePaciente").value;
+	var url = "https://web.whatsapp.com//send?";
+
+	//composição da mensagem de envio
+	// var msg1 = "&text=Confirmação de agendamento dia " + data;
+	// var msg2 = " as " + hora;
+	// var msg3 = " horas com " + medico;
+	// var msg4 = " na Alergo Dermatologia unidade " + nomeUnidade;
+	// var msg5 = " no endereço " + enderecoUnidade;
+	// var msg6 = ". Para mais informações, ligue: " + telUnidade;
+	// var mensagem = msg1 +  msg2 + msg3  + msg4  + msg5 + msg6;
+
+	//mensagem 2.0 (a acima está funcionando)
+	//%0A = nova linha;
+
+	var msgOla = "&text=Olá, %0A";
+	var msgTipoDeAtendimento = "Estamos confirmando " + tipoDeAtendimento;
+	var msgMedico = " com o(a) " + "*" + medico + "*"; //verificar os atendimentos e fazer uma list
+	var msgDia = " dia " + "*" + data + "*";
+	var msgHora = " às " + "*" + hora + "*";
+	var msgUnidade = " na unidade da Alergo Dermatologia " + "*" + nomeUnidade + "*" +".%0A";
+	var msgEndereco = "Endereço de atendimento:%0A" + "*" + enderecoUnidade  + referenciaUnidade + "*";
+	var msgOrientacoes1 = "%0A !!! Importante: %0A✔ Chegar com 15 minutos de antecedência para o atendimento na recepção. %0A";
+	var msgOrientacoes2 = "✔ No dia é obrigatório apresentar um documento com foto e carteirinha física ou digital do convênio.%0A";
+	var msgOrientacoes3 = "✔ Caso não possa comparecer, avise-nos por gentileza.%0A";
+	var msgOrientacoes4 = "✔ Este serviço está disponível apenas para confirmações. Agendamentos ou maiores informações ligue: " + telUnidade;
+
+	
+	var mensagem = msgOla + msgTipoDeAtendimento + msgMedico + msgDia + msgHora + msgUnidade + msgEndereco + msgOrientacoes1 + msgOrientacoes2 + msgOrientacoes3 + msgOrientacoes4;
+	window.open( url + telefonePaciente + mensagem);
+}
+function formatarData(data){
+	//funcionando
+	//recebe 2019-08-30 e retorna 30/08/2019
+	var ano = data.substring(0,4);
+	var mes = data.substring(5,7);
+	var dia = data.substring(8,10);
+	var dataFormatada = dia + "/" + mes + "/" + ano;
+	return dataFormatada;
+}
+
